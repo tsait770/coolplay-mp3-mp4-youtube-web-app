9f8f50281876b892f6bdc5769daebd1e91c39409
tsait770 <219434450+tsait770@users.noreply.github.com>
2025-11-25 12:59:44 -0500

    fix(voice): Correct VoiceNativeBridge.m to expose startContinuousListening method

--- ios/coolplay/VoiceNativeBridge.m
+++ ios/coolplay/VoiceNativeBridge.m
@@ -2,7 +2,7 @@
 #import <React/RCTEventEmitter.h>
 
 @interface RCT_EXTERN_MODULE(VoiceNative, RCTEventEmitter)
-RCT_EXTERN_METHOD(startListening)
+RCT_EXTERN_METHOD(startContinuousListening)
 RCT_EXTERN_METHOD(stopListening)
 @end
 
